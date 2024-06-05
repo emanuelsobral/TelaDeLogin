a23a6ac930bbe4b09a714a2ca90126df3571af72
emanuelsobral <emanuelssobral@gmail.com>
2024-06-05 00:13:52 -0300

    Fix elements | Login with Google | Organization

--- index.js
+++ index.js
@@ -48,132 +48,41 @@ window.addEventListener('resize', () => {
 
 updateEventListeners();
 
-//Validation Fields Register
-
-function validateFiels() {
-    const emailValidregister = isEmailValidregister();
-    const passwordValidregister = isPasswordValidRegister();
-    const nameRegister = isNameValidRegister();
-    document.getElementById('registerButton').disabled = !emailValidregister | !passwordValidregister | !nameRegister;
-
-    toggleRegisterError();
-}
-
-function isEmailValidregister() {
-    const emailregister = document.getElementById("emailRegister").value;
-
-    if (!emailregister) {
-        return false;
-    }
-    return validateEmailregister(emailregister);
-}
-
-function isPasswordValidRegister() {
-    const passwordRegister = document.getElementById("passwordRegister").value;
-    const confirmPasswordRegister = document.getElementById("confirmPassword").value;
-
-    if (!passwordRegister ||!confirmPasswordRegister) {
-        return false;
-    }
-    if (!passwordRegister === confirmPasswordRegister) {
-        return false;
-    }
-    if (passwordRegister === confirmPasswordRegister) {
-        return true;
-    }
-
-}
-
-function toggleRegisterError() {
-    const passwordRegister = document.getElementById("passwordRegister").value;
-    const confirmPasswordRegister = document.getElementById("confirmPassword").value;
-    const emailregister = document.getElementById("emailRegister").value;
-    const nameRegister = document.getElementById("nameRegister").value;
-
-    if ((validateEmailregister(emailregister))) {
-        document.getElementById("ErroEmail").style.display = "none";
-    } else {
-        document.getElementById("ErroEmail").style.display = "block";
-    }
-
-    if (passwordRegister === confirmPasswordRegister) {
-        document.getElementById("ErroPassword").style.display = "none";
-    } else {
-        document.getElementById("ErroPassword").style.display = "block";
-    }
-
-    if (!emailregister || !passwordValidregister || !nameRegister) {
-        document.getElementById("ErroFields").style.display = "block";
-    } else {
-        document.getElementById("ErroFields").style.display = "none";
-    }
-
-}
-
-function isNameValidRegister() {
-    const nameRegister = document.getElementById("nameRegister").value;
-
-    if (!nameRegister) {
-        return false;
-    }
-    return true;
-}
-
-function validateEmailregister(emailregister) 
-{
-    return  /\S+@\S+\.\S+/.test(emailregister);;
-}
-
-//Validation Fields Login
-
-function validateFielsLogin() {
-    const emailValidLogin = isEmailValidLogin();
-    const passwordValidLogin = isPasswordValidLogin();
-    document.getElementById('loginButton').disabled = !emailValidLogin | !passwordValidLogin;
-
-    document.getElementById('forgetPassword').disabled =!emailValidLogin;
-
-    toggleLoginError();
-}
-
-function isEmailValidLogin() {
-    const emailLogin = document.getElementById("emailLogin").value;
-
-    if (!emailLogin) {
-        return false;
-    }
-    return validateEmailLogin(emailLogin);
-}
-
-
-
-function isPasswordValidLogin() {
-    const passwordLogin = document.getElementById("passwordLogin").value;
-
-    if (!passwordLogin) {
-        return false;
-    }
-    return true;
-}
-
-function toggleLoginError() {
-    const passwordLogin = document.getElementById("passwordLogin").value;
-    const emailLogin = document.getElementById("emailLogin").value;
-
-    if (!passwordLogin ||!emailLogin) {
-        document.getElementById("ErroLogin").style.display = "block";
-    } else {
-        document.getElementById("ErroLogin").style.display = "none";
-    }
-}
-
-function validateEmailLogin(emailLogin)
- {
-    return  /\S+@\S+\.\S+/.test(emailLogin);;
-}
-
-//Paginaçao
+//autenticaçao login
 
 function Login() {
-    window.location.href = "home.html";
+    const emailLogin = document.getElementById("emailLogin").value;
+    const passwordLogin = document.getElementById("passwordLogin").value;
+
+      firebase.auth().signInWithEmailAndPassword(emailLogin, passwordLogin).then(response => {
+        window.location.href = "/pages/home.html";
+      }).catch(error => {
+          alert(getErrorMessage(error))
+      })
 }
+
+function getErrorMessage(error) {
+    switch (error.code) {
+        case 'auth/user-not-found':
+            return 'Usuário não encontrado';
+        case 'auth/wrong-password':
+            return 'Senha incorreta';
+        case 'auth/invalid-email':
+            return 'Email inválido';
+        default:
+            return 'Erro desconhecido';
+    }
+}
+
+  function signInWithGoogle() {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        window.location.href = "home.html";
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
